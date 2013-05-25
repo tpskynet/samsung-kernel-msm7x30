2a7cfd40195206deae5d635b2f924b4feed7bd11
Lars Moelleken <voku@voku-online.de>
2013-05-25 05:04:38 +0200

    + "We shouldn't try_to_freeze if locks are held."
    - lockdep: check that no locks held at freeze time -> https://github.com/franciscofranco/mako/commit/d7d5f0fb74c947c4aa81fa40f4f68492306fd7d7
    - lockdep: remove task argument from debug_check_no_locks_held -> https://github.com/franciscofranco/mako/commit/7dfaf98f3c5d1524971c8a036ddeb1ddb1e00072
    - freezer: add unsafe versions of freezable helpers for CIFS -> https://github.com/franciscofranco/mako/commit/2b8bf225dbe1716173624e9436ce298ab1864a45

--- kernel/lockdep.c
+++ kernel/lockdep.c
@@ -3877,22 +3877,23 @@ static void print_held_locks_bug(struct task_struct *curr)
 	if (debug_locks_silent)
 		return;
 
-	printk("\n=====================================\n");
-	printk(  "[ BUG: lock held at task exit time! ]\n");
-	printk(  "-------------------------------------\n");
-	printk("%s/%d is exiting with locks still held!\n",
-		curr->comm, task_pid_nr(curr));
-	lockdep_print_held_locks(curr);
-
+	printk("\n");
+	printk("=====================================\n");
+	printk("[ BUG: %s/%d still has locks held! ]\n",
+	       current->comm, task_pid_nr(current));
+	print_kernel_ident();
+	printk("-------------------------------------\n");
+	lockdep_print_held_locks(current);
 	printk("\nstack backtrace:\n");
 	dump_stack();
 }
 
-void debug_check_no_locks_held(struct task_struct *task)
+void debug_check_no_locks_held(void)
 {
-	if (unlikely(task->lockdep_depth > 0))
-		print_held_locks_bug(task);
+	if (unlikely(current->lockdep_depth > 0))
+		print_held_locks_bug();
 }
+EXPORT_SYMBOL_GPL(debug_check_no_locks_held);
 
 void debug_show_all_locks(void)
 {
